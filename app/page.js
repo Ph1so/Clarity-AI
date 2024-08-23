a4c1acc481ee012c02020b7b9286f8ef1b5cae81
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-08-23 09:42:57 -0500

    created next app with tailwind & added custom fonts

---
+++ app/page.js
@@ -0,0 +1,33 @@
+
+export default function WebPage() {
+  return (
+    <>
+      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax">
+        Chillax
+      </div>
+
+      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax font-semibold">
+        Chillax
+      </div>
+
+      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax font-extrabold">
+        Chillax
+      </div>
+
+
+      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma">
+        Axiforma
+      </div>
+
+      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma font-semibold">
+        Axiforma
+      </div>
+
+      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma font-extrabold">
+        Axiforma
+      </div>
+    </>
+
+
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        chillax: ['Chillax', 'sans-serif'], // Custom font family 1
+        axiforma: ['Axiforma', 'sans-serif'], // Custom font family 2
+      },
+      fontWeight: {
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+    },
+  },
+  plugins: [],
+};
