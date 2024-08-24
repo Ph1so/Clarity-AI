64a075a4e3b0b8f2db9d99173a5a57d2b01bd8ea
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-08-23 22:49:39 -0500

    Added navbar

--- app/page.js
+++ app/page.js
@@ -1,31 +1,20 @@
+"use client";
+import React from "react";
+import './globals.css'; 
+
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 
 export default function WebPage() {
   return (
     <>
-      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax">
-        Chillax
-      </div>
-
-      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax font-semibold">
-        Chillax
-      </div>
-
-      <div className="bg-blue-200 text-pink-500 text-center m-10 p-10 font-chillax font-extrabold">
-        Chillax
-      </div>
-
-
-      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma">
-        Axiforma
-      </div>
-
-      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma font-semibold">
-        Axiforma
-      </div>
-
-      <div className="bg-purple-200 text-pink-500 text-center m-10 p-10 font-axiforma font-extrabold">
-        Axiforma
-      </div>
+      <section id="Hero" className="flex justify-center items-center w-screen h-screen bg-gradient-to-b from-[#07052D] to-[#2C174C]"> 
+        <div className="flex flex-col space-y-10 text-center items-center">
+          <div className="font-chillax font-normal text-white"> Get Answers, Connect, and Succeed with </div>
+          <div className="font-chillax font-medium text-9xl text-white"> Clarity AI </div>
+          <button className="font-chillax font-medium text-[#17125D] bg-[#E8DBFF] rounded-full px-8 py-2 cursor-pointer hover:-translate-y-3 duration-300"> Chat with Clarity </button>
+        </div>
+      </section>
     </>
 
 
